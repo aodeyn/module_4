876bc7f149d5c0f911ba33776b15c83fd1ffca37
aodeyn <aodeyn@gmail.com>
2024-11-24 12:22:28 +0300

    Add files via upload

---
+++ true_math.py
@@ -0,0 +1,9 @@
+from math import inf
+def divide(first, second):
+    #first = input('Введите делимое : ')
+    #second = input('Введите делитель : ')
+    if second != 0:
+        div = first / second
+        return div
+    else:
+        return inf
